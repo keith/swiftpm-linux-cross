184b1175e5c303450f6c8bfcf18f6a8f277ca3ce
Keith Smiley <keithbsmiley@gmail.com>
2022-06-23 16:55:18 +0800

    Install matching toolchain

--- ci/build-swiftlint.sh
+++ ci/build-swiftlint.sh
@@ -3,11 +3,16 @@
 set -euo pipefail
 set -x
 
+wget https://download.swift.org/swift-5.6.2-release/xcode/swift-5.6.2-RELEASE/swift-5.6.2-RELEASE-osx.pkg
+installer -target ~ -pkg swift-5.6.2-RELEASE-osx.pkg
+
+toolchain_id=$(/usr/libexec/PlistBuddy -c "print :CFBundleIdentifier" ~/Library/Developer/Toolchains/swift-latest.xctoolchain/Info.plist)
+
 ./setup-toolchain
 destination=$PWD/toolchain-focal-x86_64-5.6.2/destination.json
 
 git clone --recursive --depth 1 https://github.com/realm/SwiftLint.git
 cd SwiftLint
 
-TOOLCHAINS=org.swift.562202206021a swift build --destination "$destination"
+TOOLCHAINS="$toolchain_id" swift build --destination "$destination"
 cp ./.build/debug/swiftlint ..

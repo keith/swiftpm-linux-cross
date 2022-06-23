70555ea5170815cd78b838c104ed15c3ad2ca2d6
Keith Smiley <keithbsmiley@gmail.com>
2022-06-23 17:22:38 +0800

    Fix bin path

--- ci/build-swiftlint.sh
+++ ci/build-swiftlint.sh
@@ -9,10 +9,12 @@ installer -target CurrentUserHomeDirectory -pkg swift-5.6.2-RELEASE-osx.pkg
 toolchain_id=$(/usr/libexec/PlistBuddy -c "print :CFBundleIdentifier" ~/Library/Developer/Toolchains/swift-latest.xctoolchain/Info.plist)
 
 ./setup-toolchain
-destination=$PWD/toolchain-focal-x86_64-5.6.2/destination.json
+readonly destination=$PWD/toolchain-focal-x86_64-5.6.2/destination.json
+readonly binary_path=$PWD/swiftlint
 
+cd "$(mktemp -d)"
 git clone --branch ks/switch-to-swiftpm-conditional-dependencies-api --recursive --depth 1 https://github.com/realm/SwiftLint.git
 cd SwiftLint
 
 TOOLCHAINS="$toolchain_id" swift build --destination "$destination"
-cp ./.build/debug/swiftlint ..
+cp ./.build/debug/swiftlint "$binary_path"

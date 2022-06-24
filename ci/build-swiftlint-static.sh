ee3cfd984f6ad1407ca5b386782dabe6c2160cb4
Keith Smiley <keithbsmiley@gmail.com>
2022-06-24 09:39:50 +0800

    Add static job

--- ci/build-swiftlint-static.sh
+++ ci/build-swiftlint-static.sh
@@ -8,12 +8,12 @@ readonly os=$1
 toolchain_id=$(./ci/install-macos-toolchain.sh)
 
 ./setup-toolchain --ubuntu-release "$os"
-readonly destination=$PWD/toolchain-$os-x86_64-5.6.2/destination.json
+readonly destination=$PWD/toolchain-$os-x86_64-5.6.2/destination_static.json
 readonly binary_path=$PWD/swiftlint
 
 cd "$(mktemp -d)"
 git clone --branch ks/switch-to-swiftpm-conditional-dependencies-api --recursive --depth 1 https://github.com/realm/SwiftLint.git
 cd SwiftLint
 
-TOOLCHAINS="$toolchain_id" swift build --destination "$destination"
+TOOLCHAINS="$toolchain_id" swift build --destination "$destination" --static-swift-stdlib -c release -Xlinker -lCFURLSessionInterface -Xlinker -lCFXMLInterface -Xlinker -lcurl -Xlinker -lxml2
 cp ./.build/debug/swiftlint "$binary_path"

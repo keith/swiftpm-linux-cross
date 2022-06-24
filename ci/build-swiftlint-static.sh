0940be02701cfe16ba30025f2f816b35ff17eefc
Keith Smiley <keithbsmiley@gmail.com>
2022-06-24 09:53:14 +0800

    Add arm builds

--- ci/build-swiftlint-static.sh
+++ ci/build-swiftlint-static.sh
@@ -4,10 +4,11 @@ set -euo pipefail
 set -x
 
 readonly os=$1
+readonly arch=$2
 
 toolchain_id=$(./ci/install-macos-toolchain.sh)
 
-./setup-toolchain --ubuntu-release "$os"
+./setup-toolchain --ubuntu-release "$os" --arch "$arch"
 readonly destination=$PWD/toolchain-$os-x86_64-5.6.2/destination_static.json
 readonly binary_path=$PWD/swiftlint
 
@@ -15,5 +16,5 @@ cd "$(mktemp -d)"
 git clone --branch ks/switch-to-swiftpm-conditional-dependencies-api --recursive --depth 1 https://github.com/realm/SwiftLint.git
 cd SwiftLint
 
-TOOLCHAINS="$toolchain_id" swift build --destination "$destination" --static-swift-stdlib -c release -Xlinker -lCFURLSessionInterface -Xlinker -lCFXMLInterface -Xlinker -lcurl -Xlinker -lxml2
-cp ./.build/release/swiftlint "$binary_path"
+TOOLCHAINS="$toolchain_id" swift build --destination "$destination" --static-swift-stdlib -Xlinker -lCFURLSessionInterface -Xlinker -lCFXMLInterface -Xlinker -lcurl -Xlinker -lxml2
+cp ./.build/debug/swiftlint "$binary_path"
--- ci/build-swiftlint.sh
+++ ci/build-swiftlint.sh
@@ -4,10 +4,11 @@ set -euo pipefail
 set -x
 
 readonly os=$1
+readonly arch=$2
 
 toolchain_id=$(./ci/install-macos-toolchain.sh)
 
-./setup-toolchain --ubuntu-release "$os"
+./setup-toolchain --ubuntu-release "$os" --arch "$arch"
 readonly destination=$PWD/toolchain-$os-x86_64-5.6.2/destination.json
 readonly binary_path=$PWD/swiftlint
 
